fb5779e7084a8de5180b00ac8ab0a891882b1a9a
kh4ydarov <kh4ydarovs@yandex.ru>
2024-05-16 10:53:48 +0500

    added allure reports

--- test_something.py
+++ test_something.py
@@ -1,5 +1,18 @@
 from selene import browser, have, command
+import json
+import allure
+from allure_commons.types import Severity
+from allure import attachment_type
 
+
+
+
+@allure.tag("web")
+@allure.severity(Severity.CRITICAL)
+@allure.label("owner", "Sarvar")
+@allure.feature("Задачи в репозиторий")
+@allure.story("Авторизованный пользователь может создать задачу и репозитории")
+@allure.link("https://demoqa.com", name="Testing")
 
 def test_demoqa_form():
     browser.open('/automation-practice-form')
